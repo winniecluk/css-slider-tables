89c11001c37dc3541794a194bad667f655808508
Winnie Luk <winniecluk@gmail.com>
2017-11-05 00:38:15 -0700

    delete console logs, give variable more explicit name

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 window.onload = function(){
+  // if you don't want to read the code, line 3 is the only variable you should have to change
+  var numberOfTables = 4;
   const offsetVal = 100;
   var currentIdx = 0;
-  const maxIdx = 3;
+  const maxIdx = 0 + (numberOfTables - 1);
   var mainSlide = document.querySelector('div.slides');
   function getPos(idx){
      return '-' + (offsetVal * idx).toString() + 'vw';
@@ -9,18 +11,14 @@ window.onload = function(){
 
   document.querySelector('.arrow-right').addEventListener('click', function(e){
     if (currentIdx < maxIdx){
-      console.log(currentIdx);
       currentIdx++;
-      console.log(currentIdx);
       mainSlide.style.left = getPos(currentIdx);
     }
   });
 
   document.querySelector('.arrow-left').addEventListener('click', function(e){
     if (currentIdx > 0){
-      console.log(currentIdx);
       currentIdx--;
-      console.log(currentIdx);
       mainSlide.style.left = getPos(currentIdx);
     }
   });
